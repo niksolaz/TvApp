e0f2f9ead85c5a6fc54c505f95595220e6a17cd0
Solazzo Nicola <solazzo.nicola@gmail.com>
2014-11-08 12:42:57 +0000

    added views and urls

--- remembermyseries/urls.py
+++ remembermyseries/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'remembermyseries.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^tvapp/', include('tvapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
