5969ff3d75ba46b9af24b470e877c5f69d531a74
Solazzo Nicola <solazzo.nicola@gmail.com>
2014-11-09 00:54:30 +0000

    Changed view and url

--- tvapp/urls.py
+++ tvapp/urls.py
@@ -1,4 +1,11 @@
 from django.conf.urls import patterns, include, url
 from tvapp import views
 
-urlpatterns = patterns("",url(r'^$',views.index, name='index'),)
+urlpatterns = patterns("",url(r'^$',views.index, name='index'),
+                        url(r'^(?P<username_id>)/$',views.UserView, name='UserView'),
+                        url(r'^$',views.HomeView, name='HomeView'),
+                        url(r'^(?P<tv_show_title>)/$',views.TvShowView, name='TvShowView'),
+                        url(r'^(?P<season_number>)/$',views.SeasonView, name='SeasonView'),
+                        url(r'^(?P<episode_number>)/$',views.EpisodeView, name='EpisodeView'),
+                        url(r'^$',views.LastReleases, name='LastReleases'),
+)
--- tvapp/views.py
+++ tvapp/views.py
@@ -5,3 +5,29 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hello World")
+
+def UserView(request,username_id):
+    return 'this is the user %s' % (username_id,)
+
+def HomeView(request):
+    return HttpResponse("HomeView")
+
+def TvShowView(request,tv_show_title=None):
+    if tv_show_title is None:
+        return 'this is the list of title series  ' 
+    else:
+        return 'this is the show %s' % (tv_show_title, )
+        
+def SeasonView(request,season_number=None):
+    if season_number is None:
+        list_seasons =  'this is the list of season'
+        return list_seasons
+    else:
+        return 'info about season %s' % (season_number, )
+
+def EpisodeView(request,episode_number=None):
+    list_episode = 'this is the list of episode %s'
+    return list_episode % (episode_number,)
+
+def LastReleases(request):
+    return HttpResponse("Release")
