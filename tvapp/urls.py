2ed397c049290f42226c89f4f8231e759a96e2ed
Solazzo Nicola <solazzo.nicola@gmail.com>
2014-11-09 11:33:32 +0000

    Better urls and views

--- tvapp/urls.py
+++ tvapp/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 from tvapp import views
 
-urlpatterns = patterns("",url(r'^$',views.index, name='index'),
-                        url(r'^(?P<username_id>)/$',views.UserView, name='UserView'),
-                        url(r'^$',views.HomeView, name='HomeView'),
-                        url(r'^(?P<tv_show_title>)/$',views.TvShowView, name='TvShowView'),
-                        url(r'^(?P<season_number>)/$',views.SeasonView, name='SeasonView'),
-                        url(r'^(?P<episode_number>)/$',views.EpisodeView, name='EpisodeView'),
-                        url(r'^$',views.LastReleases, name='LastReleases'),
+urlpatterns = patterns("",
+    url(r'^$',views.HomeView, name='HomeView'),
+    url(r'^users/(?P<username_id>\w+)/$',views.UserView, name='UserView'),
+    url(r'^users/$',views.UsersView, name='UsersView'),
+    url(r'^tvshows/$',views.TvShowsView, name='TvShowsView'),
+    url(r'^tvshows/last/$',views.LastReleases, name='LastReleases'),
+    url(r'^tvshows/(?P<tv_show_title>\w+)/$',views.TvShowView, name='TvShowView'),
+    url(r'^tvshows/(?P<tv_show_title>\w+)/seasons/$',views.SeasonsView, name='SeasonsView'),
+    url(r'^tvshows/(?P<tv_show_title>\w+)/seasons/(?P<season_number>\d+)/$',views.SeasonView, name='SeasonView'),
+    url(r'^tvshows/(?P<tv_show_title>\w+)/seasons/(?P<season_number>\d+)/episodes/$',views.EpisodesView, name='EpisodesView'),
+    url(r'^tvshows/(?P<tv_show_title>\w+)/seasons/(?P<season_number>\d+)/episodes/(?P<episode_number>\d+)/$',views.EpisodeView, name='EpisodeView'),
+    
 )
--- tvapp/views.py
+++ tvapp/views.py
@@ -3,31 +3,35 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("hello World")
-
-def UserView(request,username_id):
-    return 'this is the user %s' % (username_id,)
 
 def HomeView(request):
     return HttpResponse("HomeView")
 
-def TvShowView(request,tv_show_title=None):
-    if tv_show_title is None:
-        return 'this is the list of title series  ' 
-    else:
-        return 'this is the show %s' % (tv_show_title, )
-        
-def SeasonView(request,season_number=None):
-    if season_number is None:
-        list_seasons =  'this is the list of season'
-        return list_seasons
-    else:
-        return 'info about season %s' % (season_number, )
+def UsersView(request):
+    return HttpResponse('this is list of users')
 
-def EpisodeView(request,episode_number=None):
-    list_episode = 'this is the list of episode %s'
-    return list_episode % (episode_number,)
+def UserView(request,username_id):
+    return HttpResponse('this is the user %s' % (username_id,))
+
+def TvShowsView(request):
+    return HttpResponse('this is the list of tv show')
+
+def TvShowView(request,tv_show_title=None):
+    return HttpResponse('This is the tv show %s' % (tv_show_title,) )
+    
+def SeasonsView(request,tv_show_title=None):
+    return HttpResponse('This is the list of seasons for the tv show %s' % (tv_show_title,))
+      
+def SeasonView(request,tv_show_title=None, season_number=None):
+    return HttpResponse('info about season %s of %s' % (season_number, tv_show_title,))
+
+def EpisodesView(request,tv_show_title=None, season_number=None):
+    list_episode = 'this is the list of episodes for the show %s in season %s'
+    return HttpResponse(list_episode % (tv_show_title,season_number,))
+
+def EpisodeView(request,tv_show_title=None, season_number=None, episode_number=None):
+    list_episode = 'this is the episode #%s (TVShow: %s - Season: %s)'
+    return HttpResponse(list_episode % (episode_number, tv_show_title, season_number, ))
 
 def LastReleases(request):
-    return HttpResponse("Release")
+    return HttpResponse("The last releases from all the shows")
